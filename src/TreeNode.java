f735a77118b56cbf763780a409069f8f755ce8c8
Siim Aus <herrbpl@gmail.com>
2016-03-20 20:19:06 +0200

    implementing find function, not completed

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-public class TreeNode implements Iterator<TreeNode>{
+public class TreeNode implements Iterator<TreeNode> {
 
 	private String name;
 	private TreeNode firstChild;
@@ -52,6 +52,7 @@ public class TreeNode implements Iterator<TreeNode>{
 	}
 
 	public static void main(String[] param) {
+
 		String s = "A(B1,C,D)";
 		TreeNode t = TreeNode.parsePrefix(s);
 		String v = t.rightParentheticRepresentation();
@@ -70,7 +71,7 @@ public class TreeNode implements Iterator<TreeNode>{
 		return firstChild;
 	}
 
-	public void setFirstChild(TreeNode firstChild) {
+	private void setFirstChild(TreeNode firstChild) {
 		this.firstChild = firstChild;
 	}
 
@@ -78,26 +79,45 @@ public class TreeNode implements Iterator<TreeNode>{
 		return nextSibling;
 	}
 
-	public void setNextSibling(TreeNode nextSibling) {
+	private void setNextSibling(TreeNode nextSibling) {
 		this.nextSibling = nextSibling;
 	}
-	
+
 	/**
-	 * Adds children to tree. If this does not have children yet
-	 * we add child as first children, otherwise we add child as end of 
-	 * children chain
+	 * Adds children to tree. If this does not have children yet we add child as
+	 * first children, otherwise we add child as end of children chain Because
+	 * each node has only some pointers (next and down and perhaps up) we cannot
+	 * add child that has already those pointers filled, i.e. we must never add
+	 * any child instance twice to any tree. There are two strategies to deal
+	 * with that - set child pointers to null or deny adding such child. Latter
+	 * is safer but requires checking whole tree.
+	 * 
 	 * @param child
 	 */
 	public void addChild(TreeNode child) {
 		// if null, we do not add anything
-		if (child == null) return;
+		if (child == null)
+			return;
+
+		// Child must not be parent.
+		if (child == this) {
+			throw new IllegalArgumentException("Cannot add itself to children!");
+		}
+
+		if (findChild(child) != null || child.findChild(this) != null) {
+			throw new IllegalArgumentException("Cannot add child because either two tree structures overlap");
+		}
+
 		// iterator to find last existing sibling
 		Iterator<TreeNode> children = children();
-		
+
 		// if child already has some children, we should make sure none
 		// of those point back to any other member of tree to avoid loops
-		
-		
+		// nor should child itself be parent of itself
+
+		// Add parent
+		child.setParent(this);
+
 		// if this does not have children, add
 		if (children == null) {
 			setFirstChild(child);
@@ -107,27 +127,152 @@ public class TreeNode implements Iterator<TreeNode>{
 				children = children.next();
 			}
 			// add new child to be last sibling
-			((TreeNode)children).setNextSibling(child);
+			((TreeNode) children).setNextSibling(child);
 		}
+
+	}
+
+	/**
+	 * Creates node with name and adds as a child
+	 * 
+	 * @param name
+	 */
+	public void addChild(String name) {
+		addChild(new TreeNode(name, null, null));
+	}
+
+	/**
+	 * Recursively checks if child is found in structure
+	 * 
+	 * @param child
+	 *            - node to be looked for. As only one instance of node should
+	 *            exist in structure, we only need to find first
+	 * @return
+	 */
+	public TreeNode findChild(TreeNode child) {
+		// TODO Auto-generated method stub
+		Iterator<TreeNode> children = children();
+
+		// first child is child
+		if (((TreeNode) children) == child)
+			return ((TreeNode) children);
+
+		TreeNode result = null;
+
+		// no children for node
+		if (children == null)
+			return null;
+
+		// recursively look for child
+		result = ((TreeNode) children).findChild(child);
+		if (result != null)
+			return result;
+
+		// repeat for each sibling.
+		while (children.hasNext()) {
+			children = children.next();
+
+			if (((TreeNode) children) == child)
+				return ((TreeNode) children);
+
+			// no children for node
+			if (children == null)
+				return null;
+
+			// recursively look for child
+			result = ((TreeNode) children).findChild(child);
+			if (result != null)
+				return result;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Finds nth child by name
+	 * 
+	 * @param name
+	 *            - child searched for
+	 * @param position
+	 *            - nth position
+	 * @return
+	 */
+	public TreeNode find(String name, int position) {
 		
+		TreeNode result = null;		
+		int posCounter = position;
+		
+		if (this.getName() == name ) {
+			System.out.println("Find instance with name");
+			posCounter--;
+		}
+		if (posCounter <= 0) return this;
+		
+		// TODO Auto-generated method stub
+		Iterator<TreeNode> children = children();
+		
+		// no children for node
+		if (children == null)
+			return null;
+
+		// recursively look for child
+		result = ((TreeNode) children).find(name, posCounter);
+		if (result != null)
+			return result;
+
+		// repeat for each sibling.
+		while (children.hasNext()) {
+			children = children.next();
+
+			// check for child name
+			if (((TreeNode) children).getName() == name ) {
+				System.out.println("Find instance with name");
+				posCounter--;
+			}
+			if (posCounter <= 0) return ((TreeNode) children);
+			
+
+			// no children for node
+			if (children == null)
+				return null;
+
+			// recursively look for child
+			result = ((TreeNode) children).find(name, posCounter);
+			if (result != null)
+				return result;
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Adds parent to node
+	 * 
+	 * @param treeNode
+	 */
+	private void setParent(TreeNode treeNode) {
+		// TODO Auto-generated method stub
+
 	}
 
 	/**
 	 * get parent node of current node.
+	 * 
 	 * @return parent node or null
 	 */
 	public TreeNode getParent() {
 		throw new UnsupportedOperationException();
 	}
-	
+
 	public Iterator<TreeNode> children() {
 		return getFirstChild();
 	}
-	
+
 	@Override
 	public boolean hasNext() {
 		// TODO Auto-generated method stub
-		return (getNextSibling() == null);
+		return (getNextSibling() != null);
 	}
 
 	@Override
--- test/TreeNodeTest.java
+++ test/TreeNodeTest.java
@@ -1,121 +1,188 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-/** Testklass.
+/**
+ * Testklass.
+ * 
  * @author Jaanus
  */
 public class TreeNodeTest {
 
-   @Test (timeout=1000)
-   public void testParsePrefix() { 
-      String s = "A(B1,C,D)";
-      TreeNode t = TreeNode.parsePrefix (s);
-      String r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "(B1,C,D)A", r);
-      s = "2";
-      t = TreeNode.parsePrefix (s);
-      r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "2", r);
-      s = "+(*(-(2,1),4),/(6,3))";
-      t = TreeNode.parsePrefix (s);
-      r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "(((2,1)-,4)*,(6,3)/)+", r);
-   }
+	@Test(expected = RuntimeException.class)
+	public void testAddChildDouble() {
+		TreeNode root = new TreeNode("root", null, null);
+		TreeNode child1 = new TreeNode("child1", null, null);
+		root.addChild(child1);
+		
+		TreeNode test1, child2;
+		test1 = child1;
+		child1 = null;
+		child2 = null;
+		
+		for (int i = 0; i < 10; i++) {
+			child1 = new TreeNode(String.format("child%d", i), null, null);
+			for (int j = 0; j < 10; j++) {
+				child2 = new TreeNode(String.format("child%d:%d", i, j), null, null);
+				child1.addChild(child2);				
+			}
+			root.addChild(child1);
+		}
+		System.out.println("Testing double entry");
+		
+		root.addChild(test1);
+		root.addChild(child1);
+	}
 
-   @Test (timeout=1000)
-   public void testParsePrefixAndRightParentheticRepresentation1() {
-      String s = "A(B(C(D(E))))"; 
-      TreeNode t = TreeNode.parsePrefix (s);
-      String r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "((((E)D)C)B)A", r);
-      s = "A(B(C,D(E)),F)";
-      t = TreeNode.parsePrefix (s);
-      r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "((C,(E)D)B,F)A", r);
-   }
+	@Test(expected = RuntimeException.class)
+	public void testAddChildItself() {
+		TreeNode root = new TreeNode("root", null, null);
+		root.addChild(root);
+	}
 
-   @Test (timeout=1000)
-   public void testParsePrefixAndRightParentheticRepresentation2() {
-      String s = "+(*(-(512,1),4),/(-6,3))";
-      TreeNode t = TreeNode.parsePrefix (s);
-      String r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "(((512,1)-,4)*,(-6,3)/)+", r);
-      s = "A(B,C,D,E,F)";
-      t = TreeNode.parsePrefix (s);
-      r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "(B,C,D,E,F)A", r);
-      s = "6(5(1,3(2),4))";
-      t = TreeNode.parsePrefix (s);
-      r = t.rightParentheticRepresentation();
-       assertEquals ("Tree: " + s, "((1,(2)3,4)5)6", r);
-   }
+	@Test(timeout = 1000)
+	public void testfindChild() {
+		TreeNode root = new TreeNode("root", null, null);
+		TreeNode child1, child2;
+		TreeNode test1;
+		child1 = null;
+		child2 = null;
+		test1 = null;
+		for (int i = 0; i < 10; i++) {
+			child1 = new TreeNode(String.format("child%d", i), null, null);
+			for (int j = 0; j < 10; j++) {
+				child2 = new TreeNode(String.format("child%d:%d", i, j), null, null);
+				child1.addChild(child2);
+				if ( j == 5 && i == 5) {
+					test1 = child2;
+				}
+			}
+			root.addChild(child1);
+		}
+		TreeNode find = root.findChild(test1); 
+		assertTrue(String.format("child1 should equal to %s", test1.getName()), ( find == test1 ));
+		
+		find = root.find("root", 1);
+		assertNotNull("Expected to find 'root'", find);
+		assertEquals(root.getName(), find.getName());
+		
+		find = root.find("child5:5", 1);
+		assertNotNull("Expected to find 'child5:5'", find);
+		assertEquals("child5:5", find.getName());
+		
+		
+		
+		
+	}
 
-   @Test (timeout=1000)
-   public void testSingleRoot() {
-      String s = "ABC";
-      TreeNode t = TreeNode.parsePrefix (s);
-      String r = t.rightParentheticRepresentation();
-      assertEquals ("Tree: " + s, "ABC", r);
-      s = ".Y.";
-      t = new TreeNode (s, null, null);
-      r = t.rightParentheticRepresentation();
-      assertEquals ("Single node" + s, s, r);
-   } 
+	@Test(timeout = 1000)
+	public void testParsePrefix() {
+		String s = "A(B1,C,D)";
+		TreeNode t = TreeNode.parsePrefix(s);
+		String r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "(B1,C,D)A", r);
+		s = "2";
+		t = TreeNode.parsePrefix(s);
+		r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "2", r);
+		s = "+(*(-(2,1),4),/(6,3))";
+		t = TreeNode.parsePrefix(s);
+		r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "(((2,1)-,4)*,(6,3)/)+", r);
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testSpaceInTreeNodeName() {
-      TreeNode root = TreeNode.parsePrefix ("A B");
-   }
+	@Test(timeout = 1000)
+	public void testParsePrefixAndRightParentheticRepresentation1() {
+		String s = "A(B(C(D(E))))";
+		TreeNode t = TreeNode.parsePrefix(s);
+		String r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "((((E)D)C)B)A", r);
+		s = "A(B(C,D(E)),F)";
+		t = TreeNode.parsePrefix(s);
+		r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "((C,(E)D)B,F)A", r);
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testTwoCommas() {
-      TreeNode t = TreeNode.parsePrefix ("A(B,,C)");
-   }
+	@Test(timeout = 1000)
+	public void testParsePrefixAndRightParentheticRepresentation2() {
+		String s = "+(*(-(512,1),4),/(-6,3))";
+		TreeNode t = TreeNode.parsePrefix(s);
+		String r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "(((512,1)-,4)*,(-6,3)/)+", r);
+		s = "A(B,C,D,E,F)";
+		t = TreeNode.parsePrefix(s);
+		r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "(B,C,D,E,F)A", r);
+		s = "6(5(1,3(2),4))";
+		t = TreeNode.parsePrefix(s);
+		r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "((1,(2)3,4)5)6", r);
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testEmptySubtree() {
-      TreeNode root = TreeNode.parsePrefix ("A()");
-   }
+	@Test(timeout = 1000)
+	public void testSingleRoot() {
+		String s = "ABC";
+		TreeNode t = TreeNode.parsePrefix(s);
+		String r = t.rightParentheticRepresentation();
+		assertEquals("Tree: " + s, "ABC", r);
+		s = ".Y.";
+		t = new TreeNode(s, null, null);
+		r = t.rightParentheticRepresentation();
+		assertEquals("Single node" + s, s, r);
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testUnbalanced() {
-      TreeNode t = TreeNode.parsePrefix ("A,B)");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testSpaceInTreeNodeName() {
+		TreeNode root = TreeNode.parsePrefix("A B");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testInputWithoutBrackets() {
-      TreeNode t = TreeNode.parsePrefix ("A,B");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testTwoCommas() {
+		TreeNode t = TreeNode.parsePrefix("A(B,,C)");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testInputWithDoubleBrackets() {
-      TreeNode t = TreeNode.parsePrefix ("A((C,D))");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testEmptySubtree() {
+		TreeNode root = TreeNode.parsePrefix("A()");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testComma1() {
-      TreeNode root = TreeNode.parsePrefix ("A(,B)");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testUnbalanced() {
+		TreeNode t = TreeNode.parsePrefix("A,B)");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testComma2() {
-      TreeNode root = TreeNode.parsePrefix ("A(B),C(D)");
-   }
+	@Test
+	public void testInputWithoutBrackets() {
+		TreeNode t = TreeNode.parsePrefix("A,B");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testComma3() {
-      TreeNode root = TreeNode.parsePrefix ("A(B,C),D");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testInputWithDoubleBrackets() {
+		TreeNode t = TreeNode.parsePrefix("A((C,D))");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testTab1() {
-      TreeNode root = TreeNode.parsePrefix ("\t");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testComma1() {
+		TreeNode root = TreeNode.parsePrefix("A(,B)");
+	}
 
-   @Test (expected=RuntimeException.class)
-   public void testEmptyString() {
-      TreeNode root = TreeNode.parsePrefix ("");
-   }
+	@Test(expected = RuntimeException.class)
+	public void testComma2() {
+		TreeNode root = TreeNode.parsePrefix("A(B),C(D)");
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testComma3() {
+		TreeNode root = TreeNode.parsePrefix("A(B,C),D");
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testTab1() {
+		TreeNode root = TreeNode.parsePrefix("\t");
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testEmptyString() {
+		TreeNode root = TreeNode.parsePrefix("");
+	}
 
 }
-
